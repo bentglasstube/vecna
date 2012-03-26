74a175f738aafa12d20d19124cf77556480ae0c3
Alan Berndt <alan@eatabrick.org>
2012-03-26 07:41:13 -0700

    updates

--- src/org/eatabrick/vecna/Vecna.java
+++ src/org/eatabrick/vecna/Vecna.java
@@ -1,13 +1,35 @@
+/* Copyright (c) 2012 Alan Berndt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
 package org.eatabrick.vecna;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.ClipboardManager;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
@@ -25,38 +47,166 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.security.Security;
+import java.util.Iterator;
+
+import org.spongycastle.jce.provider.BouncyCastleProvider;
+import org.spongycastle.openpgp.PGPEncryptedDataList;
+import org.spongycastle.openpgp.PGPObjectFactory;
+import org.spongycastle.openpgp.PGPPrivateKey;
+import org.spongycastle.openpgp.PGPPublicKeyEncryptedData;
+import org.spongycastle.openpgp.PGPSecretKey;
+import org.spongycastle.openpgp.PGPSecretKeyRingCollection;
+import org.spongycastle.openpgp.PGPUtil;
+import org.spongycastle.openpgp.PGPLiteralData;
+import org.spongycastle.openpgp.PGPOnePassSignatureList;
+import org.spongycastle.openpgp.PGPCompressedData;
+
 
 public class Vecna extends ListActivity {
   private final static String TAG = "Vecna";
 
-  private final static int MESSAGE_DECRYPT = 0x47010001;
-
-  private ArrayList<Entry> list = new ArrayList<Entry>();
-  private BaseAdapter adapter;
+  private PasswordEntryAdapter adapter;
   private SharedPreferences settings;
+  private String passphrase = "";
+  
+  private class ReadEntriesTask extends AsyncTask<String, Integer, Integer> {
+    ProgressDialog progress;
 
-  private Handler handler = new Handler();
-  private Runnable loadEntries = new Runnable() {
-    public void run() {
-      setEmptyText(R.string.loading);
-      list.clear();
+    protected void onPreExecute() {
+      adapter.clear();
       adapter.notifyDataSetChanged();
+      adapter.setNotifyOnChange(false);
+    
+      progress = new ProgressDialog(Vecna.this);
+      progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+      progress.setMessage(getString(R.string.progress_initial));
+      progress.setCancelable(false);
+      progress.setMax(6);
 
-      if (settings.getBoolean("encrypted", true)) {
-        decryptFile();
-      } else {
-        readPasswordFile();
-      }
-      
-      adapter.notifyDataSetChanged();
+      progress.show();
     }
-  };
 
+    protected Integer doInBackground(String... string) {
+      PGPSecretKeyRingCollection keyring = null;
+      FileInputStream dataStream = null;
+
+      publishProgress(R.string.progress_keyfile);
+      try {
+        File keyFile = new File(settings.getString("key_file", ""));
+        FileInputStream keyStream = new FileInputStream(keyFile);
+        keyring = new PGPSecretKeyRingCollection(PGPUtil.getDecoderStream(keyStream));
+      } catch (Exception e) {
+        return R.string.error_key_file_not_found;
+      }
+
+      publishProgress(R.string.progress_passfile);
+      try {
+        File dataFile = new File(settings.getString("passwords", ""));
+        dataStream = new FileInputStream(dataFile);
+      } catch (Exception e) {
+        return R.string.error_password_file_not_found;
+      }
+
+      try {
+        PGPObjectFactory factory;
+
+        publishProgress(R.string.progress_find_data);
+        
+        factory = new PGPObjectFactory(PGPUtil.getDecoderStream(dataStream));
+        PGPEncryptedDataList dataList = null;
+
+        Object o;
+        while ((o = factory.nextObject()) != null) {
+          if (o instanceof PGPEncryptedDataList) {
+            dataList = (PGPEncryptedDataList) o;
+            break;
+          }
+        }
+
+        if (dataList == null) return R.string.error_no_data_in_file;
+
+        publishProgress(R.string.progress_find_key);
+
+        PGPSecretKey keySecret = null;
+        PGPPrivateKey keyPrivate = null;
+
+        PGPPublicKeyEncryptedData dataEncrypted = null;
+        Iterator it = dataList.getEncryptedDataObjects();
+        while (keyPrivate == null && it.hasNext()) {
+          dataEncrypted = (PGPPublicKeyEncryptedData) it.next();
+
+          keySecret = keyring.getSecretKey(dataEncrypted.getKeyID());
+          keyPrivate = keySecret.extractPrivateKey(string[0].toCharArray(), "BC");
+        }
+
+        if (keyPrivate == null) return R.string.error_secret_key_missing;
+
+        publishProgress(R.string.progress_decrypt);
+
+        factory = new PGPObjectFactory(dataEncrypted.getDataStream(keyPrivate, "BC"));
+        Object message = factory.nextObject();
+
+        if (message instanceof PGPCompressedData) {
+          PGPObjectFactory f = new PGPObjectFactory(((PGPCompressedData) message).getDataStream());
+          message = f.nextObject();
+        }
+
+        ByteArrayOutputStream dataFinal = new ByteArrayOutputStream();
+        if (message instanceof PGPLiteralData) {
+          InputStream s = ((PGPLiteralData) message).getInputStream();
+
+          int ch;
+          while ((ch = s.read()) >= 0) {
+            dataFinal.write(ch);
+          }
+        } else {
+          return R.string.error_encryption;
+        }
+
+        publishProgress(R.string.progress_parse);
+
+        String[] lines = dataFinal.toString().split("\n");
+        for (int i = 0; i < lines.length; ++i) {
+          adapter.add(new Entry(lines[i]));
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+        return R.string.error_unknown;
+      }
+
+      return 0;
+    }
+
+    protected void onProgressUpdate(Integer... messages) {
+      progress.setMessage(getString(messages[0]));
+      progress.incrementProgressBy(1);
+    }
+
+    protected void onPostExecute(Integer result) {
+      progress.dismiss();
+
+      adapter.setNotifyOnChange(true);
+      adapter.notifyDataSetChanged();
+
+      if (result > 0) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Vecna.this);
+
+        builder.setTitle(getString(R.string.error));
+        builder.setMessage(getString(result));
+        builder.setCancelable(false);
+        builder.setPositiveButton(android.R.string.ok, null);
+
+        builder.show();
+      }
+    }
+  }
 
   @Override public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -64,35 +214,11 @@ public class Vecna extends ListActivity {
 
     settings = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 
-    adapter = new BaseAdapter() {
-      public int getCount() {
-        return list.size();
-      }
-
-      public Object getItem(int pos) {
-        return list.get(pos);
-      }
-
-      public long getItemId(int pos) {
-        return pos;
-      }
-
-      public View getView(int pos, View view, ViewGroup viewGroup) {
-        if (view == null) {
-          view = View.inflate(Vecna.this, android.R.layout.two_line_list_item, null);
-        }
-
-        Entry entry = (Entry) getItem(pos);
-
-        ((TextView) view.findViewById(android.R.id.text1)).setText(entry.account);
-        ((TextView) view.findViewById(android.R.id.text2)).setText(entry.user);
-
-        return view;
-      }
-    };
-
+    adapter = new PasswordEntryAdapter(this);
+    adapter.setNotifyOnChange(false);
     setListAdapter(adapter);
-    getListView().setTextFilterEnabled(true);
+
+    Security.addProvider(new BouncyCastleProvider());
   }
     
   @Override public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,14 +228,13 @@ public class Vecna extends ListActivity {
   }
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
-    Intent intent = null;
     switch (item.getItemId()) {
       case R.id.settings:
-        intent = new Intent(this, Preferences.class);
+        Intent intent = new Intent(this, Preferences.class);
         startActivityForResult(intent, 0);
         return true;
       case R.id.refresh:
-        handler.post(loadEntries);
+        updateEntries();
         return true;
       default:
         return super.onOptionsItemSelected(item);
@@ -118,11 +243,11 @@ public class Vecna extends ListActivity {
 
   @Override protected void onResume() {
     super.onResume();
-    handler.post(loadEntries);
+    updateEntries();
   }
 
   @Override protected void onListItemClick(ListView parent, View v, int pos, long id) {
-    final Entry entry = list.get(pos);
+    final Entry entry = (Entry) adapter.getItem(pos);
 
     AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
@@ -171,74 +296,37 @@ public class Vecna extends ListActivity {
     dialog.show();
   }
 
-  private void setEmptyText(int resourceId) {
-    ((TextView) findViewById(android.R.id.empty)).setText(resourceId);
-  }
+  private void getPassphrase() {
+    final EditText pass = new EditText(this);
+    pass.setSingleLine();
+    pass.setTransformationMethod(new PasswordTransformationMethod());
 
-  private void readPasswordFile() {
-    try {
-      File f = new File(settings.getString("passwords", ""));
-      FileInputStream is = new FileInputStream(f);
-      BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-      String line = new String();
-      while ((line = buf.readLine()) != null) {
-        list.add(new Entry(line));
+    builder.setMessage(getString(R.string.prompt));
+    builder.setView(pass);
+    builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+      public void onClick(DialogInterface dialog, int which) {
+        Vecna.this.passphrase = pass.getText().toString();
+        new ReadEntriesTask().execute(passphrase);
       }
-    } catch (FileNotFoundException e) {
-      list.clear();
-      setEmptyText(R.string.error_file_not_found);
-    } catch (Exception e) {
-      list.clear();
-      e.printStackTrace();
-      setEmptyText(R.string.error_file_format);
-    }
+    });
+
+    builder.show();
   }
 
-  private void decryptFile() {
-    String data = "";
+  private void updateEntries() {
+    TextView empty = (TextView) findViewById(android.R.id.empty);
 
-    try {
-      File f = new File(settings.getString("passwords", ""));
-      FileInputStream is = new FileInputStream(f);
-      BufferedReader buf = new BufferedReader(new InputStreamReader(is));
-
-      String line = new String();
-      while ((line = buf.readLine()) != null) {
-        data += line + "\n";
+    if (settings.getString("passwords", "").isEmpty() || settings.getString("key_file", "").isEmpty()) {
+      empty.setText(R.string.settings);
+    } else {
+      empty.setText(R.string.empty);
+      if (passphrase.isEmpty()) {
+        getPassphrase();
+      } else {
+        new ReadEntriesTask().execute(passphrase);
       }
-
-      Intent intent = new Intent("org.thialfihar.android.apg.intent.DECRYPT_AND_RETURN");
-      intent.setType("text/plain");
-      intent.putExtra("intentVersion", "1");
-      intent.putExtra("text", data);
-
-      Log.d(TAG, "Starting decryption with data " + data);
-      startActivityForResult(intent, MESSAGE_DECRYPT);
-    } catch (FileNotFoundException e) {
-      setEmptyText(R.string.error_file_not_found);
-    } catch (Exception e) {
-      e.printStackTrace();
-      setEmptyText(R.string.error_file_format);
-    }
-  }
-
-  @Override public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    switch (requestCode) {
-      case MESSAGE_DECRYPT:
-        Log.d(TAG, "Decryption complete");
-
-        if (resultCode != RESULT_OK || data == null) {
-          Log.d(TAG, "Bad result code or data null");
-          setEmptyText(R.string.error_could_not_decrypt);
-          break;
-        } 
-
-        String[] lines = data.getStringExtra("decryptedMessage").split("\n");
-        for (int i = 0; i < lines.length; ++i) {
-          list.add(new Entry(lines[i]));
-        }
-        break;
     }
   }
 }
